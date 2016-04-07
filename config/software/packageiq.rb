213f2ac9780eb77b55e342e45a88e67ebe240cba
Eric Olsen <eric@ericolsen.net>
2016-04-07 15:46:31 -0400

    add profile.d and cron.d to package

--- config/software/packageiq.rb
+++ config/software/packageiq.rb
@@ -21,8 +21,16 @@ build do
   copy 'build/etc/packageiq/elasticsearch.json', '/etc/packageiq/elasticsearch.json'
   copy 'build/etc/packageiq/indexer.json', '/etc/packageiq/indexer.json'
 
-  if ohai['platform_version'].to_f >= 7
-    mkdir '/lib/systemd/system'
-    copy 'build/lib/systemd/system/piq_indexer.service', '/lib/systemd/system/piq_indexer.service'
+  mkdir '/etc/profile.d'
+  copy 'build/etc/profile.d/packageiq.sh', '/etc/profile.d/packageiq.sh'
+
+  mkdir '/etc/cron.d'
+  copy 'build/etc/cron.d/packageiq', '/etc/cron.d/packageiq.sh'
+
+  if ohai['platform_family'] == 'rhel'
+    if ohai['platform_version'].to_f >= 7
+      mkdir '/lib/systemd/system'
+      copy 'build/lib/systemd/system/piq_indexer.service', '/lib/systemd/system/piq_indexer.service'
+    end
   end
 end
