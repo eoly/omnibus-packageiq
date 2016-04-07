d1260f1bf57f7b6c0c9aab82b9f99ee896988867
Eric Olsen <eric@ericolsen.net>
2016-04-07 16:07:01 -0400

    added centos 7 docker build

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo 'Building Centos 6 Omnibus Package'
 docker build --rm -t omnibus-centos6-packageiq -f dockerfiles/omnibus-centos6-packageiq .
 docker run --rm -e "PACKAGEIQ_VERSION=${PACKAGEIQ_VERSION}" -e "BUILD_ITERATION=${BUILD_ITERATION}" -v `pwd`/pkg:/pkg -i -t omnibus-centos6-packageiq
+docker rmi omnibus-centos6-packageiq
+
+echo 'Building Centos 7 Omnibus Package'
+docker build --rm -t omnibus-centos7-packageiq -f dockerfiles/omnibus-centos7-packageiq .
+docker run --rm -e "PACKAGEIQ_VERSION=${PACKAGEIQ_VERSION}" -e "BUILD_ITERATION=${BUILD_ITERATION}" -v `pwd`/pkg:/pkg -i -t omnibus-centos7-packageiq
+docker rmi omnibus-centos7-packageiq
